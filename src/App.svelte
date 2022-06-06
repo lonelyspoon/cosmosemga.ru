b97187fa69a977bfc1a221b52a0dd7f4be2dbe08
icy <icy@icys-MacBook-Pro.local>
2022-06-06 21:20:25 +0300

    Menya toshnit

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+	let name = "внимание";
+</script>
+
+<main>
+	<h1>{name}!</h1>
+	<p>Даша греет кастрюли</p>
+	<h2>Спасибо за внимание</h2>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff7300;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	h2 {
+		color: #ff7300;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
