e633f9c718a0e9e76d508a6f9586c1d629108adf
icy <saveapp1337@gmail.com>
2022-06-07 21:36:59 +0300

    I have a bird

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,62 @@
 <script>
-	let name = "внимание";
+// You can use import statements here!
+//
+// More information about how to do this here:
+// https://threejs.org/docs/#manual/en/introduction/Import-via-modules
+
+import {
+  Scene,
+  PerspectiveCamera,
+  WebGLRenderer,
+  IcosahedronGeometry,
+  MeshNormalMaterial,
+  Mesh,
+} from "three";
+
+import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader.js"
+
+const scene = new Scene();
+const camera = new PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const loader = new GLTFLoader();
+
+loader.load(
+	'3D/popa.gltf',
+	function (gltf) {
+		scene.add(gltf.scene)
+	}
+)
+
+console.log(loader)
+
+
+const geometry = new IcosahedronGeometry();
+const material = new MeshNormalMaterial();
+const mesh = new Mesh(geometry, material);
+scene.add(mesh);
+
+
+
+camera.position.z = 5;
+
+const renderer = new WebGLRenderer({ alpha: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+animate();
 </script>
 
 <main>
